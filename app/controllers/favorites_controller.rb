c33e5aa5a9720d0f9a8b7977b1160414f13a7270
dai <sugerbig15@gmail.com>
2024-02-26 11:32:58 +0900

    課題提出

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,18 @@
 class FavoritesController < ApplicationController
+  # before_action :authenticate_user!
 
   def create
-    current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path
+    @blog = Blog.find(params[:blog_id])
+    @favorite = current_user.favorites.create(blog_id: params[:blog_id])
+    respond_to do |format|
+      format.js { render 'blogs/index' }
+    end
   end
 
   def destroy
-    current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path
+    @favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    respond_to do |format|
+      format.js { render 'blogs/index' }  
+    end
   end
-
 end
